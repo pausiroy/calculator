a8638910bf91715aea4f1c90a33d37fb5078ccbd
Paulo Siroy <pausiroy@gmail.com>
2022-08-26 10:00:49 -0700

    Add inputDigit and updateDisplay functions

---
+++ calculator.js
@@ -0,0 +1,47 @@
+const calculator = {
+    displayValue: '100',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
+
+function inputDigit(digit){
+    const { displayValue } = calculator;
+    calculator.displayValue= displayValue === '0' ? digit : displayValue + digit;
+}
+  
+function updateDisplay() {
+    const display = document.querySelector('.calculator-screen');
+    display.value = calculator.displayValue;
+}
+  
+updateDisplay();
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+  
+  const { target } = event;
+
+  
+  if (!target.matches('button')) {
+    return;
+  }
+
+  if (target.classList.contains('operator')) {
+    console.log('operator', target.value);
+    return;
+  }
+
+  if (target.classList.contains('decimal')) {
+    console.log('decimal', target.value);
+    return;
+  }
+
+  if (target.classList.contains('all-clear')) {
+    console.log('clear', target.value);
+    return;
+  }
+
+  inputDigit(target.value);
+  updateDisplay();
+});
